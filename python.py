0fdb19f7e46d281861e947f0a30af3aa0ea30674
Stellah Rotich <37912118+stellahrotich@users.noreply.github.com>
2018-10-23 11:48:55 +0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,35 @@
+#python revision
+"""
+
+Python is a programming language that lets you work quickly and
+ integrate systems more effectively"""
+
+print ("Hello,World!")
+
+#Commenting in python
+#we use to create single line comments in python and """ for multiple lines comments
+#example
+"""This is a
+multiline docstring."""
+#comments are used for code documantation
+#Python uses triple quotes at the beginning and end of the docstring as shwoen above
+
+print("Hello, World! Again")
+
+#Variables
+#Variables can be replaced once it has been created by assigning to another variable
+
+x = 8 # x is of type int
+x = "Sandra" # x is now of type str
+print(x)
+
+#Rules for variables in python
+
+    #A variable name must start with a letter or the underscore character
+    #A variable name cannot start with a number
+    #A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+    #Variable names are case-sensitive (age and  Age are different variables)
+
+#python concatenation
+
+#Python allows for command line input
