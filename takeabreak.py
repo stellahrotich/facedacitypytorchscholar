376c900f9730da10b67aba9ad9bb86e05464a1bf
Stellah Rotich <37912118+stellahrotich@users.noreply.github.com>
2018-11-11 15:18:29 +0300

    Create takeabreak.py
    This is a take a break prigram first commit

---
+++ takeabreak.py
@@ -0,0 +1,10 @@
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+print ("This program started at" + time.ctime())
+while (break_count < total_breaks)
+time.sleep(20)
+webbrowser.open("https://www.youtube.com/watch?v=s1CdJvPfxCY")
+break_count = break_count
